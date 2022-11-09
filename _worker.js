016217733fd700e5ce352836c1c880f71a2536a4
lianst <lianshitong@gmail.com>
2022-11-09 22:49:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="cf.281818.xyz";
+        url.hostname="www.lianst.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
